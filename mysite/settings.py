fef3823b3f521d3e05795793b58b203f225ea9d9
Jesse Backman <jesse.backman@gmail.com>
2018-11-02 15:13:56 +0200

    changed to development settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'r&z_xg3^qagkb=og1c0+fucsqddx16
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.159']
+ALLOWED_HOSTS = []
 
 
 # Application definition
